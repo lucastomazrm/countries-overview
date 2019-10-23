a3f16b662a7064b358a92725206f32857b021219
Lucas Tomaz Resende Miranda <lucas.rmiranda@ibm.com>
2019-10-23 19:11:15 -0300

    home view

---
+++ src/views/Home/index.tsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import { ApplicationState } from "../../store";
+import { Dispatch, bindActionCreators } from "redux";
+import * as CountryActions from "../../store/ducks/country/actions";
+import { connect } from "react-redux";
+
+interface DispatchProps {
+  loadAllCountries(region?: string): void;
+}
+
+type Props = DispatchProps & ApplicationState;
+
+const Home = (props: Props) => {
+  useEffect(() => {
+    if (!props.countries.data.length) {
+      props.loadAllCountries();
+    } else {
+      console.log(props.countries.data);
+    }
+  }, [props.countries.data]);
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          props.loadAllCountries();
+        }}
+      >
+        teste
+      </button>
+    </div>
+  );
+};
+
+const mapStateToProps = (state: ApplicationState) => ({ ...state });
+const mapDispatchToProps = (dispatch: Dispatch) =>
+  bindActionCreators(CountryActions, dispatch);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Home);
