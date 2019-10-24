b7e27c3dc0e66285f814fc3c6ff7a50e06ac8a0a
Lucas Tomaz Resende Miranda <lucas.rmiranda@ibm.com>
2019-10-24 19:28:26 -0300

    adjust container padding and header size

--- src/components/Container/style.ts
+++ src/components/Container/style.ts
@@ -4,8 +4,11 @@ import { ThemeType } from "../../store/ducks/theme/types";
 export const ContainerStyle = styled.div<{ theme: ThemeType }>`
   background-color: ${props => props.theme.secondaryColor};
   width: 100%;
-  height: 100%;
+  height: calc(100% - 80px);
+  padding-top: 80px;
   -webkit-transition: background-color 0.2s linear;
   -ms-transition: background-color 0.2s linear;
   transition: background-color 0.2s linear;
+  display: flex;
+  flex-direction: column;
 `;
--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -4,20 +4,24 @@ import { ThemeType } from "../../store/ducks/theme/types";
 export const HeaderStyle = styled.div<{ theme: ThemeType }>`
   background-color: ${props => props.theme.primaryColor};
   height: 80px;
+  width: 100%;
+  position: absolute;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
-  padding: 0 5rem;
-  -webkit-transition: background-color 0.2s linear;
-  -ms-transition: background-color 0.2s linear;
   transition: background-color 0.2s linear;
+  box-shadow: 0 0 10px #00000026;
 `;
 
 export const TitleStyle = styled.h2<{ theme: ThemeType }>`
   font-size: 1.5rem;
   color: ${props => props.theme.text};
   font-weight: 800;
+  padding: 0 5rem;
+  @media(max-width: 812px) {
+    padding: 0 1rem;
+  }
 `;
 
 export const ThemeSelectorStyle = styled.button<{ theme: ThemeType }>`
@@ -27,6 +31,10 @@ export const ThemeSelectorStyle = styled.button<{ theme: ThemeType }>`
   color: ${props => props.theme.text};
   background: transparent;
   cursor: pointer;
+  padding: 0 5rem;
+  @media(max-width: 812px) {
+    padding: 0 1rem;
+  }
   svg {
     margin-right: 10px;
   }
