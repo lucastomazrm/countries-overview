23fc16c73bb3469b459f44ca4bcedbe440e0a869
Lucas Tomaz Resende Miranda <lucas.rmiranda@ibm.com>
2019-10-25 00:32:24 -0300

    create country cards

---
+++ src/components/CountryCard/index.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import {
+  CardStyle,
+  FlagStyle,
+  CountryDetails,
+  CountryTitle,
+  OtherInfo
+} from "./style";
+import { ApplicationState } from "../../store";
+import { connect } from "react-redux";
+import { Country } from "../../store/ducks/country/types";
+
+interface OwnProps {
+  country: Country;
+}
+
+type Props = OwnProps & ApplicationState;
+
+const CountryCard = (props: Props) => (
+  <CardStyle color={props.theme.primaryColor}>
+    <FlagStyle src={props.country.flag} />
+    <CountryDetails>
+      <CountryTitle color={props.theme.text}>{props.country.name}</CountryTitle>
+      <OtherInfo color={props.theme.text}>
+        Population: <span>{props.country.population.toLocaleString()}</span>
+      </OtherInfo>
+      <OtherInfo color={props.theme.text}>
+        Region: <span>{props.country.region}</span>
+      </OtherInfo>
+      <OtherInfo color={props.theme.text}>
+        Capital: <span>{props.country.capital}</span>
+      </OtherInfo>
+    </CountryDetails>
+  </CardStyle>
+);
+
+const mapStateToProps = (state: ApplicationState) => ({ ...state });
+
+export default connect(mapStateToProps)(CountryCard);
---
+++ src/components/CountryCard/style.ts
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+import { ThemeType } from "../../store/ducks/theme/types";
+
+export const CardStyle = styled.div<{ color: string }>`
+  background-color: ${props => props.color};
+  width: 18%;
+  min-width: 200px;
+  margin: 0 40px 80px 40px;
+  box-shadow: 0 0 10px #00000026;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: all 0.3s;
+
+  :hover {
+    box-shadow: 0px 15px 25px -5px #0000001a;
+    transform: scale(1.03);
+  }
+`;
+
+export const FlagStyle = styled.img`
+  width: 100%;
+  height: 150px;
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
+`;
+
+export const CountryDetails = styled.div`
+  padding: 25px;
+  display: flex;
+  flex-direction: column;
+  background: transparent;
+`;
+
+export const CountryTitle = styled.span<{ color: string }>`
+  font-weight: 800;
+  color: ${props => props.color};
+  margin-bottom: 20px;
+  font-size: 16px;
+`;
+
+export const OtherInfo = styled.span<{ color: string }>`
+  font-weight: 600;
+  color: ${props => props.color};
+  font-size: 14px;
+  padding: 5px 0;
+`;
